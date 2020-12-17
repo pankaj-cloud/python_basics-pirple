81ce1405b96469939e3883625e1bb876d5572907
Pankaj K Pathania <65821916+pankaj-cloud@users.noreply.github.com>
2020-12-17 16:50:52 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,18 @@
+"""
+String concatenation,
+..lets print a string "Welcome to_________ "
+"""
+# place = "himachal"
+# print("Welcome to " + place)
+# print("Welcome to {}".format(place))
+# print(f"Welcome to {place} ")
+
+
+adjective = input("Adjective :")
+verb1 = input("verb1 : ")
+verb2 = input("verb 2 : ")
+famous_place = input('favorite tourist destination :')
+
+madlib = f"Himachal is such a {adjective}. You can so many activities here and enjoy nature.\
+ i love to do {verb1} and {verb2}. {famous_place} is my favorite place in Himachal ."
+print(madlib)
